0a813e9666b7709d03a65dad98ac8e9270b61931
root <root@hepgrid92.ph.liv.ac.uk>
2019-06-12 14:37:11 +0100

    add GPU as a constant for coprocessor (but it may be omitted)

--- test/test.sh
+++ test/test.sh
@@ -4,6 +4,8 @@
 # BTW: jsonschema is a python tool you can install...
 
 jsonschema -i liv.json ../ccr/schema.json   2>&1  | sed -e "s/^/\n\n/" -e "s/,/,\n/g" > liv.results
-jsonschema -i man.json ../ccr/schema.json   2>&1  | sed -e "s/^/\n\n/" -e "s/,/,\n/g" > man.results
-jsonschema -i gla.json ../ccr/schema.json   2>&1  | sed -e "s/^/\n\n/" -e "s/,/,\n/g" > gla.results
+
+#jsonschema -i liv.json ../ccr/schema.json   2>&1  | sed -e "s/^/\n\n/" -e "s/,/,\n/g" > liv.results
+#jsonschema -i man.json ../ccr/schema.json   2>&1  | sed -e "s/^/\n\n/" -e "s/,/,\n/g" > man.results
+#jsonschema -i gla.json ../ccr/schema.json   2>&1  | sed -e "s/^/\n\n/" -e "s/,/,\n/g" > gla.results
 
