63ba2a39582f1dbd399f4ad437371cb546a6c967
root <root@hepgrid92.ph.liv.ac.uk>
2019-06-12 13:12:21 +0100

    Fix the test, and do up the manchester schema to make it work.

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-# jsonschema -i liv.json ../ccr/schema.json 
-jsonschema -i man.json ../ccr/schema.json 
+# Test the json files I know about. The sed at the end makes it pretty. If the results are empty, you're good.
+# BTW: jsonschema is a python tool you can install...
+
+jsonschema -i liv.json ../ccr/schema.json   2>&1  | sed -e "s/^/\n\n/" -e "s/,/,\n/g" > liv.results
+jsonschema -i man.json ../ccr/schema.json   2>&1  | sed -e "s/^/\n\n/" -e "s/,/,\n/g" > man.results
+jsonschema -i gla.json ../ccr/schema.json   2>&1  | sed -e "s/^/\n\n/" -e "s/,/,\n/g" > gla.results
 
