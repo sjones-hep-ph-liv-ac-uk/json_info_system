56807fbe7e6766d16d521b65d28789c08fe6c28b
root <root@hepgrid92.ph.liv.ac.uk>
2019-06-19 15:44:22 +0100

    Checks relational aspects (multiplicities, uniqueness...) of the crr json files

--- v1.5/code/simpleParser.py
+++ v1.5/code/simpleParser.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
-
-import json
 import getopt, sys
-import collections
 import ijson
 
 def die(s):
@@ -15,18 +12,15 @@ def usage():
 def getExtrinsics(bigList,smallList):
   # The contract is: all in small list must exist in big list.
   extrinsics = ""
-  if (not bigList):
-    return ""
-  else:
-    for small in smallList:
-      itExists = False 
-      for big in bigList:
-        #print 'compare ' + small + ' ' + big        
-        if (small == big):
-          itExists = True
-      if (itExists != True):
-        extrinsics = extrinsics + small + " "
-    return extrinsics
+  for small in smallList:
+    itExists = False 
+    for big in bigList:
+      #print 'compare ' + small + ' ' + big        
+      if (small == big):
+        itExists = True
+    if (itExists != True):
+      extrinsics = extrinsics + small + " "
+  return extrinsics
 
 #----
 def main():
@@ -40,7 +34,7 @@ def main():
   for opt, arg in opts:
     if opt in ("-h", "--help"):
       usage()
-      sys.exit(2)
+      sys.exit(3)
     elif opt in ("-j", "--json"):
       jsonFile = arg
     else:
@@ -52,124 +46,148 @@ def main():
   resourceLevelVOs = []
   resourceLevelShares = []
   resourceLevelAps = []
-
   sharesLevelVOs = []
   apsLevelShares = []
 
-  level = -1
-  
   inArrayOfResourceVOs = False
   inArrayOfShareVOs = False
+  inArrayOfApsShares = False
   inShares = False
-  inAccessPoints = False
-
-  resource = 'none'
+  inAps = False
 
   parser = ijson.parse(f)
 
-  for prefix, event, value in parser:
-    #print "\n\nEVENT: "
-    #print "prefix: ",prefix
-    #print "event: ",event
-    #print "value: ",value
+  currentLevel = -1
+  currentResource = 'none'
 
-    # Only value allowed on level 0 is computingresources 
-    if (level == 0):
-      if (event == 'map_key'):
-        if (value != 'computingresources'):
-          die('First tag must be computingresources')
-
-    # The resource name comes on level 1. Only thing to check is uniqueness
-    if (level == 1):
-      if (event == 'map_key'):
-        resource = value
-        if (resource in allResources):
-          die("duplicate resource " + resource)
-
-        # Starting a new resource, so set everything up
-        allResources.append(resource)
-        del resourceLevelVOs[:]
-        del sharesLevelVOs[:]
-        del resourceLevelShares[:]
-        del resourceLevelAps[:]
+  try:
+    for prefix, event, value in parser:
+  
+      #print "\n\nEVENT: "
+      #print "prefix: ",prefix
+      #print "event: ",event
+      #print "value: ",value
+  
+      # Only value allowed on level 0 is computingresources 
+      if (currentLevel == 0):
+        if (event == 'map_key'):
+          if (value != 'computingresources'):
+            die('First tag must be computingresources')
+  
+      # The resource name comes on level 1. Only thing to check is uniqueness.
+      if (currentLevel == 1):
+        if (event == 'map_key'):
+          currentResource = value
+          if (currentResource in allResources):
+            die("duplicate resource " + currentResource)
+  
+          # Starting a new resource, so set everything up
+          allResources.append(currentResource)
+          del resourceLevelVOs[:]
+          del sharesLevelVOs[:]
+          del apsLevelShares[:]
+          del resourceLevelShares[:]
+          del resourceLevelAps[:]
+  
+          inArrayOfResourceVOs = False
+          inArrayOfShareVOs = False
+          inArrayOfApsShares = False
+          inShares = False
+          inAps = False
+  
+      if (currentLevel == 2):
+      
+        if (event == 'end_map'):
+          # When exiting a resource make sure no VOs in the shares are extrinsic to the VOs in the resource section (if any)
+          if (resourceLevelVOs):
+            problemVOs = getExtrinsics(resourceLevelVOs,sharesLevelVOs)
+            if (len(problemVOs) != 0):
+              die ('inconsistent assigned VOs in resource ' + currentResource + ', check resource and share VOs for ' + problemVOs )
+  
+          # When exiting a resource make sure no shares in the aps are extrinsic to the shares in the resource section (if any)
+          if (resourceLevelShares):
+            problemShares = getExtrinsics(resourceLevelShares,apsLevelShares)
+            if (len(problemShares) != 0):
+              die ('inconsistent shares for resource ' + currentResource + ', check shares for ' + problemShares)
+        
+        elif (event == 'map_key'):
+          # Identify the section we are entering; assigned_vos, shares or accesspoints
+          if (value == 'assigned_vos'):
+            inArrayOfResourceVOs = True
+            inAps = False
+            inArrayOfApsShares = False
+            inArrayOfShareVOs = False
+            inShares = False
+          elif (value == 'shares'):
+            inShares = True
+            inAps = False
+            inArrayOfApsShares = False
+            inArrayOfResourceVOs = False
+            inArrayOfShareVOs = False
+          elif (value == 'accesspoints'):
+            inAps = True
+            inArrayOfApsShares = False
+            inArrayOfResourceVOs = False
+            inArrayOfShareVOs = False
+            inShares = False
+  
+      if (currentLevel == 3):
+        # make sure names are unique
+        if (inShares):
+          if (event == 'map_key'):
+            share = value 
+            if (share in resourceLevelShares):
+              die("duplicate share " + share + " in resource " + currentResource)
+            resourceLevelShares.append(share)
+  
+        if (inAps):
+          if (event == 'map_key'):
+            ap = value 
+            if (ap in resourceLevelAps):
+              die("duplicate accesspoint " + ap + " in resource " + currentResource)
+            resourceLevelAps.append(ap)
+  
+      if (currentLevel == 4):
+        # Find out if we should be collecting, e.g. share level VOs or aps level shares.
+        if (event == 'map_key'):
+          if (inShares):
+            if (value == 'assigned_vos'):
+              inArrayOfShareVOs = True
+              inAps = False 
+          elif (inAps):
+            if (value == 'shares'):
+              inArrayOfApsShares = True
+              inShares = False
+  
+      if (inArrayOfResourceVOs):
+        if (event == 'string'):
+          resourceLevelVOs.append(value)
+  
+      if (inArrayOfShareVOs):
+        if (event == 'string'):
+          sharesLevelVOs.append(value)
+  
+      if (inArrayOfApsShares):
+        if (event == 'string'):
+          apsLevelShares.append(value)
+  
+      # Finish any arrays in one go so I don't need to woory
+      if (event == 'end_array'):
         inArrayOfResourceVOs = False
         inArrayOfShareVOs = False
-        inAccessPoints = False
-        inShares = False
-        inAccessPoints = False
+        inArrayOfApsShares = False
+  
+      # Work the level 
+      if (event == 'start_map'):
+        currentLevel = currentLevel + 1
+      elif (event == 'end_map'):
+        currentLevel = currentLevel - 1
 
-    if (level == 2):
-    
-      # When exiting level 2, make sure no VOs in the shares are extrinsic to the VOs in the resource section (if any)
-      if (event == 'end_map'):
-        if (resourceLevelVOs):
-          problemVO = getExtrinsics(resourceLevelVOs,sharesLevelVOs)
-          if (len(problemVO) != 0):
-            die ('inconsistent assigned VOs in ' + resource + ', check resource and share VOs for ' + problemVO )
-      
-      elif (event == 'map_key'):
-        # Identify the section we are entering; assigned_vos, shares or accesspoints
-        if (value == 'assigned_vos'):
-          inArrayOfResourceVOs = True
-          inAccessPoints = False
-          inShares = False
-          inAccessPoints = False
-          inArrayOfShareVOs = False
-        elif (value == 'shares'):
-          inShares = True
-          inAccessPoints = False
-          inArrayOfResourceVOs = False
-          inArrayOfShareVOs = False
-        elif (value == 'accesspoints'):
-          inAccessPoints = True
-          inArrayOfResourceVOs = False
-          inArrayOfShareVOs = False
-          inShares = False
-
-    if (level == 3):
-      # make sure names are unique
-      if (inShares):
-        if (event == 'map_key'):
-          share = value 
-          if (share in resourceLevelShares):
-            die("duplicate share " + share + " in resource " + resource)
-          resourceLevelShares.append(share)
-
-      if (inAccessPoints):
-        if (event == 'map_key'):
-          ap = value 
-          if (ap in resourceLevelAps):
-            die("duplicate accesspoint " + ap + " in resource " + resource)
-          resourceLevelAps.append(ap)
-
-    if (level == 4):
-      # Find out if we should be collecting share level VOs
-      if (event == 'map_key'):
-        if (value == 'assigned_vos'):
-          inArrayOfShareVOs = True
-          inArrayOfResourceVOs = False
-          inAccessPoints = False 
-
-    # Collect resource level VOs
-    if (inArrayOfResourceVOs):
-      if (event == 'string'):
-        resourceLevelVOs.append(value)
-
-    # Collect share level VOs
-    if (inArrayOfShareVOs):
-      if (event == 'string'):
-        sharesLevelVOs.append(value)
-
-    # Finish any arrays
-    if (event == 'end_array'):
-      inArrayOfResourceVOs = False
-      inArrayOfShareVOs = False
-
-    # Work the level 
-    if (event == 'start_map'):
-      level = level + 1
-    elif (event == 'end_map'):
-      level = level - 1
+  except ijson.backends.python.UnexpectedSymbol as e:
+    print 'Some kind of parse error: ' + str(e)
+    print 'Perhaps json file is not well formed.'
+    print 'Please validate the json _before_ using this program.'
+    sys.exit(4)
 
 if __name__ == "__main__":
     main()
